4b4e73f29b0ebed0d6152b96d6c362f80007dd7a
honzatomek <rpi3.tomek@protonmail.com>
2021-12-29 21:49:28 +0100

    python3.10.1 installation script

---
+++ bin/install_python3.10.1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
+
+version=3.10.1
+
+wget https://www.python.org/ftp/python/$version/Python-$version.tgz
+
+tar zxf Python-$version.tgz
+cd Python-$version
+./configure --enable-optimizations
+make -j4
+sudo make altinstall
+
